c1a6f8e46df5dafed62557ff0355930f5ff38796
Luciano Bessegato <lfrbessegato@gmail.com>
2019-09-25 23:41:33 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -126,6 +128,10 @@ public class SellerFormController implements Initializable {
 		}
 	}
 
+	/* 
+	 *  Pega os dados do formulário e 
+	 *  carrega um objeto com esses dados e retornado o objeto no final.
+	 */
 	private Seller getFormData() {
 		Seller obj = new Seller();
 
@@ -134,15 +140,41 @@ public class SellerFormController implements Initializable {
 
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
 
+		// Valida o Name
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addError("name", "Field can´t be empty");
 		}
-
+		obj.setName(txtName.getText());
+		
+		// Valida o Email
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can´t be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+		
+		// Para obter o valor do DatePicker() e validar o BirthDate
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can´t be empty");
+		}
+		else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant)); // Para converter o instante para Data
+		}
+		
+		//Validar BaseSalary
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can´t be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+		
+		//Validar Departamento
+		obj.setDepartment(comboBoxDepartment.getValue());
+		
 		// Verifica se na coleção tem pelo menos 1 erro
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
-		obj.setName(txtName.getText());
+		
 
 		return obj;
 	}
@@ -197,11 +229,40 @@ public class SellerFormController implements Initializable {
 
 	}
 
+	// Mostra nos Label´s correspondentes os seus respectivos erros.
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
 
+		// Errors == Name
 		if (fields.contains("name")) {
 			labelErrorName.setText(errors.get("name"));
+		}
+		else {
+			labelErrorName.setText("");
+		}
+		
+		// Errors = Email
+		if (fields.contains("email")) {
+			labelErrorEmail.setText(errors.get("email"));
+		}
+		else {
+			labelErrorEmail.setText("");
+		}
+		
+		// Errors = baseSalary
+		if (fields.contains("baseSalary")) {
+			labelErrorBaseSalary.setText(errors.get("baseSalary"));
+		}
+		else {
+			labelErrorBaseSalary.setText("");
+		}
+		
+		// Errors = birthDate
+		if (fields.contains("birthDate")) {
+			labelErrorBirthDate.setText(errors.get("birthDate"));
+		}
+		else {
+			labelErrorBirthDate.setText("");
 		}
 	}
 
